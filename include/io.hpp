87637f3b94c46f1d952f5b04c4b888484c46f3e8
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-04 14:49:59 -0500

    Update after moving some utility functions from hf to here.

--- include/io.hpp
+++ include/io.hpp
@@ -16,11 +16,9 @@ namespace io {
 */
 void readArrayFromFile(const std::string& filename, Eigen::MatrixXd& M);
 
-
 /** Read an array from a given filename line by line, and add the elements to a given tensor (rank-4 tensor)
 */
 void readArrayFromFile(const std::string& filename, Eigen::Tensor<double, 4>& M);
-
 
 /** Print the contents of a rank-four tensor in a fashionable way
  */
--- include/linalg.hpp
+++ include/linalg.hpp
@@ -15,6 +15,18 @@ namespace linalg {
  */
 bool areEqual(const Eigen::Tensor<double, 4>& M, const Eigen::Tensor<double, 4>& T, const double tolerance);
 
+/** Check if two sets of eigenvalues are equal
+ */
+bool areEqualEigenvalues(Eigen::VectorXd evals1, Eigen::VectorXd evals2, double tol);
+
+/** Check if two eigenvectors are equal. This is the case if they are equal up to their sign.
+ */
+bool areEqualEigenvectors(Eigen::VectorXd evec1, Eigen::VectorXd evec2, double tol);
+
+/** Check if two sets of eigenvectors are equal.
+ */
+bool areEqualSetsOfEigenvectors(Eigen::MatrixXd evecs1, Eigen::MatrixXd evecs2, double tol);
+
 
 }  // namespace linalg
 }  // namespace cpputil
