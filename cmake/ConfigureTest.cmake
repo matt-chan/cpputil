c6ac0380629281d4e3248c28f2848ecb6e0156f8
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-04-25 13:56:28 -0400

    Update comments.

--- cmake/ConfigureTest.cmake
+++ cmake/ConfigureTest.cmake
@@ -1,9 +1,9 @@
-# In this CMake file, we will include header files and link to libraries for a given test source.
+# In this CMake file, we will include header files and link to libraries for a given test source
 
 
-# Include Boost.
+# Include Boost
 target_include_directories(${TEST_NAME} PUBLIC ${Boost_INCLUDE_DIRS})
 
-# Include this project.
+# Include this project
 target_include_directories(${TEST_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
 target_link_libraries(${TEST_NAME} PRIVATE ${LIBRARY_NAME})
--- cmake/FindPackages.cmake
+++ cmake/FindPackages.cmake
@@ -1,8 +1,8 @@
 # In this CMake file, we will find all required packages
 
 
-# Find Boost.
+# Find Boost
 find_package(Boost REQUIRED)
 
-# Find Eigen3 (see http://eigen.tuxfamily.org/dox/TopicCMakeGuide.html).
+# Find Eigen3 (through the supplied FindEigen3.cmake file
 find_package(Eigen3 3.3.4 REQUIRED)
