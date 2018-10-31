e02f0536de57e745fc56dff520ca7c2bdc974006
Matthew Chan <chanmhy@mcmaster.ca>
2018-10-31 17:14:56 -0400

    fixed linking to librt

--- cmake/ConfigureLibrary.cmake
+++ cmake/ConfigureLibrary.cmake
@@ -5,7 +5,7 @@
 target_include_directories(${LIBRARY_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
 
 # Include Eigen3
-target_link_libraries(${LIBRARY_NAME} PUBLIC Eigen3::Eigen)
+target_link_libraries(${LIBRARY_NAME} PUBLIC Eigen3::Eigen rt)
 
 # Include Boost
 target_include_directories(${LIBRARY_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
