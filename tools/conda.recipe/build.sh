714e2a6a8fbea810a9e1cb2c55dd8c6fa0f0ea68
Matthew Chan <chanmhy@mcmaster.ca>
2018-11-01 00:13:32 -0400

    use run_exports to let other get library imports, move library to standard location if env is set.

--- tools/conda.recipe/build.sh
+++ tools/conda.recipe/build.sh
@@ -2,5 +2,5 @@
 
 mkdir build_release
 cd build_release
-cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DLIBRARY_TYPE=SHARED .. 
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPROJECT_INSTALL_DIR=${PREFIX} -DLIBRARY_TYPE=SHARED .. 
 make && make test && make install
