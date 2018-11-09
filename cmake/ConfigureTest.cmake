7a153f7876d777f1f0383270672d125cfa655b8f
guacke <Guillaume.Acke@UGent.be>
2018-11-09 16:59:21 +0100

    Bug fix: error in copy.

--- cmake/ConfigureTest.cmake
+++ cmake/ConfigureTest.cmake
@@ -9,5 +9,5 @@ target_include_directories(${TEST_NAME} PRIVATE ${Boost_INCLUDE_DIRS})
 target_link_libraries(${TEST_NAME} PRIVATE ${Boost_LIBRARIES})
 
 # Include Eigen3
-target_link_libraries(${LIBRARY_NAME} PUBLIC Eigen3::Eigen)
+target_link_libraries(${TEST_NAME} PUBLIC Eigen3::Eigen)
 
